03b5e47f2a3477fda7739566df24013d463d0646
CR-Daniel <choudaniel3@gmail.com>
2024-03-24 00:14:02 +0100

    created build
    .exe file

--- README.md
+++ README.md
@@ -9,9 +9,10 @@
 python main.py
 ```
 
-### TODO
+### Build
 
-* Make "Start/Stop Data Collection" button functional by calling the low-level functions just like in the blog.
-* After selecting a file, its name should be displayed below the button for example.
-* Green theming to make it look nice and sustainable.
-* Not only show power consumption over time, but also indicate total power consumption and then converted to equivalent CO2 emissions.
+```python
+pyinstaller --onefile --windowed main.py
+```
+
+The executable file will be in the `dist` folder.
--- services/e3/get_csv.py
+++ services/e3/get_csv.py
@@ -5,10 +5,22 @@ def get_csv():
     try:
         # Construct the path to the Downloads folder dynamically
         downloads_dir = os.path.join(os.environ['USERPROFILE'], 'Downloads')
+        csv_file_path = os.path.join(downloads_dir, 'srumutil.csv')  # Assuming the output file is named 'srumutil.csv'
         
-        # Run the command and direct output to the Downloads folder
+        # Ensure any existing instance of the file is removed to avoid confusion
+        if os.path.exists(csv_file_path):
+            os.remove(csv_file_path)
+        
+        # Run the command
         subprocess.check_call(['powercfg.exe', '/srumutil'], cwd=downloads_dir)
         
-        print(f"CSV file generated successfully in {downloads_dir}.")
+        # Check if the file was created
+        if os.path.exists(csv_file_path):
+            print(f"CSV file generated successfully at {csv_file_path}.")
+            return csv_file_path  # Return the path to the generated CSV file
+        else:
+            print("Failed to generate CSV file: File does not exist.")
+            return None
     except subprocess.CalledProcessError as e:
         print("Failed to generate CSV file:", e)
+        return None
