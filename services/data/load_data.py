1433daed71082ad20ae188f8a1a88347f1212915
Jan-Hendrik Schneider <j-h.schneider@outlook.de>
2024-03-24 17:29:54 +0100

    Shorten the appid

--- services/data/load_data.py
+++ services/data/load_data.py
@@ -4,4 +4,6 @@ def load_data(file_path):
     data = pd.read_csv(file_path)
     data.columns = data.columns.str.strip()
     data['TimeStamp'] = pd.to_datetime(data['TimeStamp'].str.strip(), format='%Y-%m-%d:%H:%M:%S.%f')
+    # Extract the short AppIDs
+    data['AppId'] = data['AppId'].str.split('\\').str[-1].str.split('[').str[0].str.replace('.exe', '')
     return data
